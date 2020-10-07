b3c845415a93c6e245d7a2ce4e504d825abe9ac0
Fajr <72368235+Beyonder12@users.noreply.github.com>
2020-10-07 09:58:09 +0700

    Update hospital.js
    testing

--- common/models/hospital.js
+++ common/models/hospital.js
@@ -1,6 +1,6 @@
 // Copyright IBM Corp. 2014,2016. All Rights Reserved.
 // Node module: loopback-getting-started-intermediate
-// This file is licensed under the MIT License.
+// This file is licensed under the MIT License.jj
 // License text available at https://opensource.org/licenses/MIT
 
 'use strict';
